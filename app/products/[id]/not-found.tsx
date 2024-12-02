d366094ee53c503637c74cfd46015da150e19bf7
Ruru232 <sanchez.rolando1998@yahoo.com>
2024-12-02 13:59:54 +0800

    Fixed: error-handling

--- app/products/[id]/not-found.tsx
+++ app/products/[id]/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className="flex flex-col justify-center items-center text-black font-jost">
+    <div className="flex flex-col justify-center items-center text-black font-jost mt-96 mb-[550px]">
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
     </div>
